fe4548d6c3e774a53208ed9b42d93f8d9636ef1e
Josh Faigan <djjoker13@gmail.com>
2024-06-28 20:58:30 -0400

    trying new approach to loading k8 file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,11 @@ ls -R
 # Export KUBECONFIG if not already set
 export KUBECONFIG=${KUBECONFIG:-~/.kube/config}
 
+# Debugging
+kubectl config view
+
 # Namespace
 NAMESPACE="anonymoose-prod"
-
-# Add a unique annotation to the deployment file to force Kubernetes to see it as updated
-unique_id=$(date +%s)
-
-# Update the deployment file with a unique annotation
-sed -i "s/annotations:.*/annotations:\n      redeploy-hash: \"$unique_id\"/" deployment/production/app_deployment.yml
 
 # Apply Kubernetes manifests
 kubectl apply -f deployment/production/memcache_deployment.yml -n $NAMESPACE
